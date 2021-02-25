9bdf534175f1d765e2b9f9e334a191885e3a7f68
piscesweilun <piscesweilun@gmail.com>
2021-02-25 11:10:26 +0800

    peerjs server放在google cloud VM

--- http-server.js
+++ http-server.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || "8000";
 app.use(express.static(path.join(__dirname)));
 
 app.get("/", (request, response) => {
-    response.sendFile(__dirname + "/sender.html");
+    response.sendFile(__dirname + "/receiver.html");
 });
 
-app.get("/receiver", (request, response) => {
-    response.sendFile(__dirname + "/receiver.html");
+app.get("/sender", (request, response) => {
+    response.sendFile(__dirname + "/sender.html");
 });
 server.listen(port);
 console.log('Listening on: ' + port);
